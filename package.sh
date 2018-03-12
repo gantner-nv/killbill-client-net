6e4e2188033749a5c1fc43d8a26896c994ed28f5
Dieter Mijle <dieter.mijle@syx.be>
2018-03-12 12:23:43 +0100

    [Maintenance] Relese beta version

--- package.sh
+++ package.sh
@@ -1,2 +1,2 @@
 sudo rm -r ./temp
-sudo dotnet pack src/KillBill.Client.Net/KillBill.Client.Net.csproj --include-symbols -o ../temp > pack.KillBill.Client.Net.log
+sudo dotnet pack src/KillBill.Client.Net/KillBill.Client.Net.csproj --include-symbols -o ../../temp > pack.KillBill.Client.Net.log
